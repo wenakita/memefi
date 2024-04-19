c4575cc9c0e2ed3ef8a22988c3f49cee3e066398
exoticsolutions <rafajr1263@gmail.com>
2024-04-19 19:31:36 -0400

    more adding

--- web3modal-quickstart/src/components/main/Layout.tsx
+++ web3modal-quickstart/src/components/main/Layout.tsx
@@ -14,7 +14,7 @@ function Layout() {
   return (
     <div className="container">
       <center>
-        <NavigationMenu className="border border-slate-500 p-3 bg-black rounded-xl">
+        <NavigationMenu className="border border-slate-400 p-3 bg-black rounded-xl">
           <NavigationMenuList className="">
             <NavigationMenuItem className="">
               <HoverCard>
@@ -23,17 +23,17 @@ function Layout() {
                     <AvatarImage src="https://dd.dexscreener.com/ds-data/tokens/base/0xddf7d080c82b8048baae54e376a3406572429b4e.png?size=lg&key=18ea46" />
                   </Avatar>
                 </HoverCardTrigger>
-                <HoverCardContent className="w-80 bg-stone-900">
+                <HoverCardContent className="w-80 bg-black rounded-xl border-black">
                   <div className="flex justify-between space-x-4">
                     <Avatar>
-                      <AvatarImage src="./public/GoddogwifShadow.webp" />
+                      <AvatarImage src="https://dd.dexscreener.com/ds-data/tokens/base/0xddf7d080c82b8048baae54e376a3406572429b4e.png?size=lg&key=18ea46" />
                     </Avatar>
                     <div className="space-y-1">
                       <h4 className="text-sm font-semibold">@GodDog</h4>
                       <p className="text-sm">
                         The future of omnichain memecoins. For teh tech
                       </p>
-                      <div className="flex items-center pt-2">
+                      <div className="flex justify-center pt-2 ">
                         <a
                           href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e"
                           className="text-xs text-stone-100"
